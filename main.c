8600892419c1cf0cecc8845ff5b5143c12784d97
ASEZ1 <asmaaezzine96@gmail.com>
2023-07-26 09:59:19 -0700

    all

--- main.c
+++ main.c
@@ -7,6 +7,7 @@
  * @env: An array of environment variables.
  * Return: Returns 0 on successful execution.
  */
+
 int main(int ac, char **av, char **env)
 {
 	char *buf = NULL;
@@ -14,117 +15,54 @@ int main(int ac, char **av, char **env)
 	char **argums = NULL;
 	pid_t pid;
 	int status, x_character;
+	int inter = 1;
 
 	(void)ac;
 	(void)av;
-
+	if (!isatty(STDIN_FILENO))
+	{
+		inter = 0;
+	}
 	while (1)
 	{
-		write(1, "$ ", 2);
+		if (inter)
+		{
+			write(1, "$ ", 2);
+		}
 		x_character = getline(&buf, &buf_size, stdin);
 		if (x_character == -1)
 		{
-			write(1, "\n", 1);
+			if (inter)
+			{
+				write(1, "\n", 1);
+			}
 			break;
 		}
 		argums = str_split(buf, " \t\n");
 		if (compare_string_strcmp(argums[0], "exit") == 0)
 		{
-			free_tokens(argums);
+			tok_free(argums);
 			break;
 		}
-		else if (compare_string_strcmp(argums[0], "Execute") == 0)
+		pid = fork();
+		if (pid == 0)
 		{
-			if (argums[1] && compare_string_strcmp(argums[1], "/bin/ls") == 0)
-			{
-				pid = fork();
-				if (pid == 0)
-				{
-					comnd_exec(argums + 1, env);
-				}
-				else
-				{
-					wait(&status);
-				}
-			}
-			else if (argums[1] && compare_string_strcmp(argums[1], "/bin/ls") == 0 && argums[2] && atoi(argums[2]) > 0)
-			{
-				int times = atoi(argums[2]);
-				for (int i = 0; i < times; i++)
-				{
-					pid = fork();
-					if (pid == 0)
-					{
-						comnd_exec(argums + 1, env);
-					}
-					else
-					{
-						wait(&status);
-					}
-				}
-			}
-			else if (argums[1] && compare_string_strcmp(argums[1], "/bin/ls") == 0 && argums[2] && compare_string_strcmp(argums[2], "times") == 0)
-			{
-				int times = atoi(argums[3]);
-				for (int i = 0; i < times; i++)
-				{
-					pid = fork();
-					if (pid == 0)
-					{
-						comnd_exec(argums + 1, env);
-					}
-					else
-					{
-						wait(&status);
-					}
-				}
-			}
-			else if (argums[1] && compare_string_strcmp(argums[1], "only") == 0 && compare_string_strcmp(argums[2], "(small)") == 0)
-			{
-				printf("Small spaces: \n");
-			}
-			else if (argums[1] && compare_string_strcmp(argums[1], "only") == 0 && compare_string_strcmp(argums[2], "(large)") == 0)
-			{
-				printf("Large spaces: \n");
-			}
-			else if (argums[1] && compare_string_strcmp(argums[1], "only") == 0 && compare_string_strcmp(argums[2], "(medium)") == 0)
-			{
-				printf("Medium spaces: \n");
-			}
-			else if (argums[1] && compare_string_strcmp(argums[1], "a") == 0)
-			{
-				printf("Executing a command that fails...\n");
-				pid = fork();
-				if (pid == 0)
-				{
-					comnd_exec(argums + 1, env);
-				}
-				else
-				{
-					wait(&status);
-				}
-			}
-			else
-			{
-				printf("Invalid command format\n");
-			}
+
+			comnd_exec(argums, env);
+			perror("Error executing command"); 
+			exit(EXIT_FAILURE); 
+		}
+		else if (pid < 0)
+		{
+
+			perror("Fork error");
 		}
 		else
 		{
-			pid = fork();
-			if (pid == 0)
-			{
-				comnd_exec(argums, env);
-			}
-			else
-			{
-				wait(&status);
-			}
+			wait(&status);
+			tok_free(argums);
 		}
-
-		free_tokens(argums);
 	}
 	free(buf);
 	return (0);
 }
-
